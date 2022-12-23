ea81ae6866971d787e2131d8c8f0164f2d92cd67
Felipe P <fpalmasaldivia@gmail.com>
2022-12-22 22:02:47 -0300

    se agregan archivos .js y .json

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const fs = require("fs")
+const PORT = 3000;
+
+
+
 app.listen(3000, console.log("¡Servidor encendido!"))
+
+
 app.get("/home", (req, res) => {
     res.send("Hello World Express Js")
 })
