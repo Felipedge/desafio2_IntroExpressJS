c48e9bb5aa8a30b65774a80e6ddf22f50b95d5c8
Felipe P <fpalmasaldivia@gmail.com>
2022-12-22 21:20:23 -0300

    Second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+app.listen(3000, console.log("¡Servidor encendido!"))
+app.get("/home", (req, res) => {
+    res.send("Hello World Express Js")
+})
